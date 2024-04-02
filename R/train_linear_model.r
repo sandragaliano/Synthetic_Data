ee1fe8060800313bee8e0e7228dbb5eed612e182
Sandra <146835558+sandragaliano@users.noreply.github.com>
2024-04-02 16:50:08 +0200

    Create train_linear_model.r

---
+++ R/train_linear_model.r
@@ -0,0 +1,7 @@
+# Model Training ---------------------------------------------------------
+train_linear_model <- function(data) {
+  # Lineal Regression 
+  formula <- as.formula(paste("Outcome ~", paste(names(data)[-which(names(data) == "Outcome")], collapse = " + ")))
+  model <- lm(formula, data = data)
+  return(model)
+}
