3a3fd9eb31e1ebf609c8511529ca190266d311cd
Sandra <146835558+sandragaliano@users.noreply.github.com>
2024-04-03 08:31:59 +0200

    Delete R directory

--- R/train_linear_model.r
+++
@@ -1,7 +0,0 @@
-# Model Training ---------------------------------------------------------
-train_linear_model <- function(data) {
-  # Lineal Regression 
-  formula <- as.formula(paste("Outcome ~", paste(names(data)[-which(names(data) == "Outcome")], collapse = " + ")))
-  model <- lm(formula, data = data)
-  return(model)
-}
